877cdb6481e824b21cd24fae238ce479a870e166
Devyn Piri <31304405+mrAsh4r@users.noreply.github.com>
2024-04-14 17:04:25 +0500

    init

---
+++ Laba1_client/Laba1_client/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+
+struct Message
+{
+    public string Sender { get; set; }
+    public string Text { get; set; }
+}
+class NamedPipeClient
+{
+    static async Task Main(string[] args)
+    {
+        string senderName = GetSenderName();
+
+        try
+        {
+            // Подключаемся к именованному каналу сервера
+            using (var client = new NamedPipeClientStream(".", "MyNamedPipe", PipeDirection.InOut))
+            {
+                Console.WriteLine("Подключение к серверу...");
+                await client.ConnectAsync();
+                Console.WriteLine("Подключение установлено.");
+
+
+                while (true)
+                {
+                    if (await RecvMsg(client)) {
+                        continue;
+                    };
+
+                    // Отправляем ответное сообщение серверу
+                    Console.Write("Введите ответное сообщение для сервера: ");
+                    string response = Console.ReadLine();
+                    SendMsg(client, senderName, response);
+                   
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+    }
+    static async Task<bool> RecvMsg(NamedPipeClientStream con) {
+        // Читаем сообщение от сервера
+        byte[] buffer = new byte[4096];
+        int bytesRead = await con.ReadAsync(buffer, 0, buffer.Length);
+
+        if (bytesRead == 0)
+            return true;
+
+        Message receivedMessage = GetMessageFromBytes(buffer, bytesRead);
+        //string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+        Console.WriteLine("(Сервер) [" + receivedMessage.Sender + "] >> " + receivedMessage.Text);
+
+        return false;
+    }
+        
+    static async void SendMsg(NamedPipeClientStream con, string senderName, string response) {
+        Message messageToSend = new Message { Sender = senderName, Text = response };
+
+        byte[] responseBytes = GetMessageBytes(messageToSend);
+        // byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+        await con.WriteAsync(responseBytes, 0, responseBytes.Length);
+    }
+
+    static string GetSenderName()
+    {
+        Console.Write("Введите имя отправителя: ");
+        return Console.ReadLine();
+    }
+
+    static byte[] GetMessageBytes(Message message)
+    {
+        string messageJson = JsonSerializer.Serialize(message);
+        return Encoding.UTF8.GetBytes(messageJson);
+    }
+
+    static Message GetMessageFromBytes(byte[] buffer, int length)
+    {
+        string messageJson = Encoding.UTF8.GetString(buffer, 0, length);
+        return JsonSerializer.Deserialize<Message>(messageJson);
+    }
+}
---
+++ Laba1_server/Laba1_server/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+struct Message
+{
+    public string Sender { get; set; }
+    public string Text { get; set; }
+}
+
+class NamedPipeServer
+{
+    static async Task Main(string[] args)
+    {
+        string senderName = GetSenderName();
+
+        try
+        {
+            // Создаем именованный канал
+            using (var server = new NamedPipeServerStream("MyNamedPipe"))
+            {
+                Console.WriteLine("Ожидание подключения клиента...");
+                await server.WaitForConnectionAsync();
+                Console.WriteLine("Клиент подключен.");
+
+                while (true)
+                {
+                    // Отправляем сообщение клиенту
+                    Console.Write("Введите сообщение для клиента: ");
+                    string text = Console.ReadLine();
+                    SendMsg(server, senderName, text);
+
+
+                    if (await RecvMsg(server))
+                    {
+                        continue;
+                    };
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+    }
+
+    static async Task<bool> RecvMsg(NamedPipeServerStream con)
+    {
+        // Читаем сообщение от сервера
+        byte[] buffer = new byte[4096];
+        int bytesRead = await con.ReadAsync(buffer, 0, buffer.Length);
+
+        if (bytesRead == 0)
+            return true;
+
+        Message receivedMessage = GetMessageFromBytes(buffer, bytesRead);
+        //string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+        Console.WriteLine("(Клиент) [" + receivedMessage.Sender + "] >> " + receivedMessage.Text);
+
+        return false;
+    }
+
+    static async void SendMsg(NamedPipeServerStream con, string senderName, string response)
+    {
+        Message messageToSend = new Message { Sender = senderName, Text = response };
+
+        byte[] responseBytes = GetMessageBytes(messageToSend);
+        // byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+        await con.WriteAsync(responseBytes, 0, responseBytes.Length);
+    }
+
+    static string GetSenderName()
+    {
+        Console.Write("Введите имя отправителя: ");
+        return Console.ReadLine();
+    }
+
+    static byte[] GetMessageBytes(Message message)
+    {
+        string messageJson = JsonSerializer.Serialize(message);
+        return Encoding.UTF8.GetBytes(messageJson);
+    }
+
+    static Message GetMessageFromBytes(byte[] buffer, int length)
+    {
+        string messageJson = Encoding.UTF8.GetString(buffer, 0, length);
+        return JsonSerializer.Deserialize<Message>(messageJson);
+    }
+}
